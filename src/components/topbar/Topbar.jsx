d5a1d66659fe7ad494df96072345844745f15e3b
KamilHH <wlad.bart12@gmail.com>
2022-05-02 11:38:23 +0200

    Topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './topbar.scss'
+import {Mail, PhoneAndroid} from "@material-ui/icons";
 
-const Topbar = () => {
+const Topbar = ({menuOpen, setMenuOpen}) => {
     return (
-        <div className="topbar">
-
+        <div className={"topbar " + (menuOpen && "active") }>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="" className="logo">portfolio.</a>
+                    <div className="itemContainer">
+                        <PhoneAndroid className="icon"/>
+                        <span>+48 123 456 789</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>kamilwozniak.js@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
