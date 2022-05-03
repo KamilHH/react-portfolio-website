c8715d31140c535df0c801381fc928d1986ce560
KamilHH <wlad.bart12@gmail.com>
2022-05-03 14:15:36 +0200

    Intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './intro.scss'
+import {KeyboardArrowDown} from "@material-ui/icons";
 
 const Intro = () => {
     return (
         <div className="intro" id="home">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Kamil Woźniak</h1>
+                    <h3>Frontend Developer</h3>
+                </div>
+                <a href="#portfolio">
+                    <KeyboardArrowDown/>
+                </a>
 
+            </div>
         </div>
     );
 };
