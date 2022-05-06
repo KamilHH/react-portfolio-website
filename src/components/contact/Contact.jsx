51443db3d69f0ee7e923b26c03a49c0c145ea84d
KamilHH <wlad.bart12@gmail.com>
2022-05-06 15:10:15 +0200

    Testimonials section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,31 @@
+import './contact.scss';
+import Image from '../../assets/shake.svg'
+import {useState} from "react";
 
-import './contact.scss'
 const Contact = () => {
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setMessage(true)
+    }
+
     return (
         <div className="contact" id="contact">
-
+            <div className="left">
+                <img src={Image} alt=""/>
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit} >
+                    <input type="text" placeholder="E-MAIL"/>
+                    <textarea placeholder="Message">
+                    </textarea>
+                    {message && <span> Thanks for your message.
+                        We will get back with you ASAP :)</span>}
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     );
 };
